5003c6bc1d5e25e942d33af3b77ed7e4565c1d79
Narin Sundarabhaya <nrs710@gmail.com>
2017-02-21 17:48:48 -0800

    Updated templates and components

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'accounts'
 urlpatterns = [ # <- any urls specific to this app should go here. helps prevent duplicate urls
     #url(r'^', views.signup_view, name='Login'),
-    url(r'^$', views.base_view, name="main"),
-    url(r'^home/', views.base_view, name='home'),
+    url(r'^$', views.index_view, name="main"),
+    url(r'^home/', views.index_view, name='home'),
     url(r'^login', views.login_view, name='login'),
     url(r'^signup/', views.signup_view, name='signup'),
     url(r'^invalid/', views.invalid_view, name='invalid'),
